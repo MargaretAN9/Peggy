18a184dd1befceab853c25df9c05df8a730d30f2
MargaretAN9 <37722313+MargaretAN9@users.noreply.github.com>
2018-06-07 02:47:45 +0000

    USB camera takes video and picture

---
+++ usbvideopygamecopy3.py
@@ -0,0 +1,41 @@
+# code originally from https://gist.github.com/snim2/255151
+
+
+import pygame
+import pygame.camera
+from pygame.locals import *
+
+DEVICE = '/dev/video0'
+SIZE = (640, 480)
+
+FILENAME = 'capture.png'
+
+
+
+def camstream():
+    pygame.init()
+    pygame.camera.init()
+    display = pygame.display.set_mode(SIZE, 0)
+    camera = pygame.camera.Camera(DEVICE, SIZE)
+    camera.start()
+    screen = pygame.surface.Surface(SIZE, 0, display)
+    capture = True
+    while capture:
+        screen = camera.get_image(screen)
+        display.blit(screen, (0,0))
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                capture = False
+            elif event.type == KEYDOWN and event.key == K_s:
+                pygame.image.save(screen, FILENAME)
+                camera.stop()
+                pygame.quit()
+                return
+
+    camera.stop()
+    pygame.quit()
+    return
+
+if __name__ == '__main__':
+    camstream()
