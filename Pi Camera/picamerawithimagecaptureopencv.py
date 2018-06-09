c66aa7b33cec1aaf9427060df5a72c770e6a3c36
MargaretAN9 <37722313+MargaretAN9@users.noreply.github.com>
2018-06-09 00:57:44 +0000

    Displays vid + captures pic

---
+++ Pi Camera/picamerawithimagecaptureopencv.py
@@ -0,0 +1,49 @@
+# shows video and captures image using picmaera and opencv
+# from https://www.pyimagesearch.com/2015/03/30/accessing-the-raspberry-pi-camera-with-opencv-and-python/ 
+#  
+# press q to quit 
+
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+
+#set filename/resolution
+#resolution size 4:3 options: (1920,1088),(1640,1232),(640,480)
+# note (3280,2464) provides 'out of resources' 
+
+SIZE = (640,480)
+FILEOUT = '/home/pi/Desktop/testimage1.jpg'
+
+ 
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (SIZE)
+camera.framerate = 5
+rawCapture = PiRGBArray(camera, size=(SIZE))
+                                    
+ 
+# allow the camera to warmup
+time.sleep(0.1)
+ 
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# grab the raw NumPy array representing the image, then initialize the timestamp
+	# and occupied/unoccupied text
+	image = frame.array
+ 
+	# show the frame
+	cv2.imshow("Frame", image)
+	key = cv2.waitKey(1) & 0xFF
+
+ 
+	# clear the stream in preparation for the next frame
+	rawCapture.truncate(0)
+ 
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+cv2.imwrite(FILEOUT,image)
+camera.close()
+cv2.waitKey(0)
